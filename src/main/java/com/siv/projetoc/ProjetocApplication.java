c70c36990bc78d9596264d4913f189d5705fde8b
Felipe Kellermann <blue_stahli@hotmail.com>
2026-03-19 20:35:02 -0300

    Initial commit

---
+++ src/main/java/com/siv/projetoc/ProjetocApplication.java
@@ -0,0 +1,13 @@
+package com.siv.projetoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siv/projetoc/ProjetocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siv.projetoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
