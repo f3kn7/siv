160c52511b9f77dcac3378b16e05af6109916577
Felipe Kellermann <blue_stahli@hotmail.com>
2026-03-19 21:22:29 -0300

    feat: adiciona feature habilidade

--- src/main/java/com/siv/projetoc/ProjetocApplication.java
+++ src/main/java/com/siv/projetoc/ProjetocApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetocApplication.class, args);
+    }
 
 }
